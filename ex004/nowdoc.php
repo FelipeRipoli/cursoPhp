eca38932700169b0a7d0c28ba54d2f74d50d1dbb
Felipe Ripoli <feliperipoli3@gmail.com>
2023-10-25 00:21:16 -0300

    Estudo e Teste de Manipulação de Strings, Escapes Sequences, Heredoc e Nowdoc

---
+++ ex004/nowdoc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Nowdoc</title>
+</head>
+<body>
+    <h1>Sintaxe Nowdoc</h1>
+    <?php 
+        $canal = "Curso em vídeo";
+        $ano = date('Y');
+
+        echo <<< 'TESTE'
+            Olá galera do $canal!
+                Tudo bem com vocês?
+            Como está sendo esse ano de $ano?
+                Abraços! \u{1F596}        
+        TESTE;
+    ?>
+</body>
+</html>
