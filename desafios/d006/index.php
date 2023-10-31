3cd0f3102f7c3a3bf3c90e59cddeb4be0ac1eeea
Felipe Ripoli <feliperipoli3@gmail.com>
2023-10-31 09:22:35 -0300

    Desafio de mostrar na tela todos 'elementos' de uma divisão (divisor, dividendo, quociente e resto) sendo com um formulário 'retroalimentado', ou seja, aparecer na mesma tela, sem ter que abrir outra página.

---
+++ desafios/d006/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Desafio PHP</title>
+</head>
+<body>
+    <?php 
+        $valor1 = $_POST['v1'] ?? 1;
+        $valor2 = $_POST['v2'] ?? 1;
+    ?>
+    <main>
+        <h1>Anatomia de uma Divisão</h1>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+            <label for="v1">Dividendo</label>
+            <input type="number" name="v1" id="v1" value="<?=$valor1?>">
+            <label for="v2">Divisor</label>
+            <input type="number" name="v2" id="v2" value="<?=$valor2?>">
+            <input type="submit" value="Analisar">
+        </form>
+    </main>
+
+    <section>
+        <h2> Estrututra da Divisão </h2>
+        <?php 
+            $conta = $valor1 / $valor2;
+            $resto = $valor1 % $valor2;
+            echo "O <i>dividendo</i> é <strong>" . $valor1 . "</strong>, o <i>divisor</i> é <strong>" . $valor2 . "</strong>, o <i>resultado/ quociente</i> da divisão é <strong>" . number_format($conta, 0) . "</strong>, e o <i>resto</i> da divisão é <strong>" . $resto . "</strong>.";
+        ?>
+    </section>
+</body>
+</html>
