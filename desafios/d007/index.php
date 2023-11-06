ce64026a286c44856322e251baee0cf7889495d4
Felipe Ripoli <feliperipoli3@gmail.com>
2023-11-06 09:07:00 -0300

    Desafio de calcular quantos salários mínimos existem dentro de um número que o usuário digitar + a o valor que sobraria dos salários mínimos.

--- desafios/d007/index.php
+++ desafios/d007/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,20 +7,22 @@
     <title>Desafio PHP 007</title>
 </head>
 <body>
+    <?php 
+        $salM = 1380.60;
+    ?>
     <main>
         <h1>Informe seu Salário</h1>
         <form action="<?php echo $_SERVER['PHP_SELF']?> " method="post">
             <label for="sal">Salário (R$)</label>
-            <input type="number" name="sal" id="sal">
-            <p>Considerando o salário mínimo de <strong>R$1.380,00</strong></p>
+            <input type="number" name="sal" id="sal" value="" step="0.01">
+            <p>Considerando o salário mínimo de <strong>R$<?=number_format($salM, 2, ",",".") ?>.</strong></p>
             <input type="submit" value="Calcular">
         </form>
     </main>
     <section>
         <?php 
-            $sal = $_POST['sal'] ?? 0;
-            $salM = 1380;
-            $div = $sal / $salM;
+            $sal = $_POST['sal'] ?? 1380.60;
+            $div = floor($sal / $salM);
             $rest = $sal % $salM;
 
             echo "<h2>Resultado Final</h2>";
