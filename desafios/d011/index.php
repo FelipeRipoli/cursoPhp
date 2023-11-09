62e09309f18e18d3b058c4afd402d8d8792ff045
Felipe Ripoli <feliperipoli3@gmail.com>
2023-11-09 09:34:48 -0300

    Desafio em PHP de criar um 'Reajustador de Preços', ou seja, o usuário final define um valor/ preço e em seguida define uma porcentagem % para que o preço seja reajustado. Por exemplo, um preço de 10$ com um reajuste de 10% que passará a custar 11$.

--- desafios/d011/index.php
+++ desafios/d011/index.php
@@ -7,27 +7,38 @@
     <title>Desafio PHP 011</title>
 </head>
 <body>
+    <?php 
+        $preco = $_POST['preco'] ?? 0;
+        $reajuste = $_POST['reajuste'] ?? 0;
+    ?>
     <main>
         <h1>Reajustador de Preços</h1>
         <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
             <label for="preco">Preço do Produto (R$)</label>
-            <input type="number" name="preco" id="preco">
-            <label for="reajuste">Qual será o percentual de reajuste?</label>
-            <input type="number" name="reajuste" id="reajuste">
+            <input type="number" name="preco" id="preco" min="0.10" step="0.01" value="<?=$preco?>">
+
+            <label for="reajuste">Qual será o percentual de reajuste? (<strong><span id="p">?</span>%</strong>)</label>
+            <input type="range" name="reajuste" id="reajuste" min="0" max="100" step="1" oninput="mudaValor()" value="<?=$reajuste?>">
+
             <input type="submit" value="Reajustar">
         </form>
     </main>
     <section>
         <h2>Resultado do Reajuste</h2>
         <?php 
-            $preco = $_POST['preco'] ?? 00;
-            $reajuste = $_POST['reajuste'] ?? 00;
-
-            $porc = ($reajuste / 100) * $preco;
+            $porc = $preco * $reajuste / 100;
             $conta = $preco + $porc;
 
-            echo "O produto que custava R$" . number_format($preco, 2, ",") . ", com <strong>" .  number_format($reajuste, 0) . "% de aumento</strong> vai passar a custar <strong>R$" . number_format($conta, 2, ",") . "</strong> a partir de agora.";
+            echo "O produto que custava R$" . number_format($preco, 2, ",", ".") . ", com <strong>" .  number_format($reajuste, 0) . "% de aumento</strong> vai passar a custar <strong>R$" . number_format($conta, 2, ",",".") . "</strong> a partir de agora.";
         ?>
     </section>
+    <script>
+        //DECLARACOES AUTOMATICAS
+        mudaValor()
+
+        function mudaValor(){
+            p.innerText = reajuste.value;
+        }
+    </script>
 </body>
 </html>
