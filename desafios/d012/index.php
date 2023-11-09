17ce326a427c7105382d98056612c4cbc72a6b4b
Felipe Ripoli <feliperipoli3@gmail.com>
2023-11-09 09:35:36 -0300

    Desafio PHP consiste em pegar um número de segundos definido pelo usuário final, e com esse tempo mostrar quantas semanas, dias, horas, minutos e segundos existem dentro desse tempo definido pelo usuário, utilizando divisão e resto da divisão.

--- desafios/d012/index.php
+++ desafios/d012/index.php
@@ -7,32 +7,43 @@
     <title>Desafio PHP 012</title>
 </head>
 <body>
+    <?php 
+        $total = $_POST['segs'] ?? 00;
+    ?>
     <main>
         <h1>Calculadora de Tempo</h1>
         <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
         <label for="segs">Qual é o total de segundos?</label>
-        <input type="number" name="segs" id="segs">
+        <input type="number" name="segs" id="segs" min="0" step="1" required value="<?=$total?>">
         <input type="submit" value="Calcular">
         </form>
     </main>
     <section>
         <h2>Totalizando Tudo</h2>
         <?php 
-            $segs = $_POST['segs'] ?? 00;
+        $sobra = $total;
+        // Total de Semanas
+        $semana = (int)($sobra / 604_800);
+        $sobra = $sobra % 604_800;
+        // Total de Dias
+        $dia = (int)($sobra / 86_400);
+        $sobra = $sobra % 86_400;
+        // Total de Horas
+        $hora = (int)($sobra / 3_600);
+        $sobra = $sobra % 3_600;
+        // Total de Minutos
+        $minuto = (int)($sobra / 60);
+        $sobra = $sobra % 60;
+        // Total de Segundos
+        $segundo = $sobra;
 
-            $semanas = floor($segs / 604800);
-            $dias = floor($segs / 86400) % 7;
-            $horas = floor($segs / 3600) % 24;
-            $minutos = floor($segs / 60) % 60;
-            $segundos = ($segs / 60);
-
-            echo "Analisando o valor que você digitou, <strong>" . number_format($segs, 0, ".") . "</strong> equivalem a um total de:";
+            echo "Analisando o valor que você digitou, <strong>" . number_format($total, 0, ",", ".") . "</strong> equivalem a um total de:";
             echo "<ul>
-                <li>". $semanas . " semanas</li>
-                <li>" . $dias . " dias</li>
-                <li>" . $horas . " horas</li>
-                <li>" . $minutos . " minutos</li>
-                <li>" . number_format($segundos, 0) . " segundos</li>
+                <li>". $semana . " semanas</li>
+                <li>" . $dia . " dias</li>
+                <li>" . $hora . " horas</li>
+                <li>" . $minuto . " minutos</li>
+                <li>" . number_format($segundo, 0) . " segundos</li>
                 </ul>";
         ?>
     </section>
